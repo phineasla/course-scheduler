bbbf648779da4bea11b0e98c7acf5fa32e82b215
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-09-28 23:24:19 +0700

    Finish header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Timetable } from "./components/Timetable";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Course Scheduler</h1>
+      <header className="App-header border">
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-2">
+          <div className="flex-row font-semibold text-xl">
+            <div>Course Scheduler</div>
+          </div>
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </nav>
       </header>
       <body>
         <Timetable />
       </body>
+      <footer></footer>
     </div>
   );
 }
