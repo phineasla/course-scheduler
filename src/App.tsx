9a71282e98950cd3feb1d8cce7112dc034a05790
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-09-28 19:08:06 +0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Timetable } from "./components/Timetable";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Course Scheduler</h1>
       </header>
+      <body>
+        <Timetable />
+      </body>
     </div>
   );
 }
---
+++ src/components/Timetable.tsx
@@ -0,0 +1,34 @@
+
+// const columns: GridColDef[] = [
+//   { field: "id", headerName: "Course ID", width: 100 },
+//   { field: "name", headerName: "Course Name", width: 200 },
+//   { field: "credit", headerName: "Credit", width: 50 },
+//   { field: "lecture", headerName: "Lecture", width: 150 },
+//   { field: "lab", headerName: "Lab", width: 150 },
+// ];
+
+const colFields = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
+const colHeaderNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let width = 150;
+
+for (let i = 0; i < colFields.length; i++) {
+  const colField = colFields[i];
+  const colHeaderName = colHeaderNames[i];
+}
+
+export const Timetable = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
