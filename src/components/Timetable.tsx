a4a3222ae6e4fff6d30616fb0aa2f3b8f048927b
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-09-29 12:51:23 +0700

    Basic timetable using <table>, consider using <li> for column-like coding

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -1,4 +1,3 @@
-
 // const columns: GridColDef[] = [
 //   { field: "id", headerName: "Course ID", width: 100 },
 //   { field: "name", headerName: "Course Name", width: 200 },
@@ -7,8 +6,7 @@
 //   { field: "lab", headerName: "Lab", width: 150 },
 // ];
 
-const colFields = ["sun", "mon", "tues", "wed", "thurs", "fri", "sat"];
-const colHeaderNames = [
+const colHeader = [
   "Sunday",
   "Monday",
   "Tuesday",
@@ -18,17 +16,28 @@ const colHeaderNames = [
   "Saturday",
 ];
 
-let width = 150;
-
-for (let i = 0; i < colFields.length; i++) {
-  const colField = colFields[i];
-  const colHeaderName = colHeaderNames[i];
-}
+const timeColWidth = 32;
 
 export const Timetable = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col mx-2 md:mx-6">
+      <div className="flex-grow overflow-auto">
+        <table className="table-fixed relative w-full border">
+          <tr>
+            <th className="sticky top-0 px-6 py-3 w-32 border border-gray" />
+            {colHeader.map((value, index, array) => (
+              <th className="sticky top-0 px-6 py-3 w-32 border border-gray">
+                {value}
+              </th>
+            ))}
+          </tr>
+        </table>
+      </div>
+      {/* <div className="flex overflow-auto justify-center">
+        {colHeader.map((value, index, array) => (
+          <div className="border border-gray w-32">{value}</div>
+        ))}
+      </div> */}
     </div>
   );
 };
